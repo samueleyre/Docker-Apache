832e7ab4fbf125403434c0566eed241073535dce
Édouard Touraille <etouraille@ns3028384.ip-149-202-90.eu>
2016-05-05 17:40:28 +0200

    Add stop and restart container

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,13 @@
 container='opnadminpreprod_apache_1';
 env='staging'
 
+sudo rm -Rf html
+
+sudo docker stop ${container}
+
 git clone --recursive git@github.com:OpnKitchenTeam/opnAdmin.git html
+
+sudo docker-compose up -d
 
 sudo docker exec -ti ${container} npm install
 sudo docker exec -ti ${container} bower install --allow-root
